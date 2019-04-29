2f36dff7149687a53a5d01c949a4e48bdce085cf
annemck <annekh88@gmail.com>
2019-04-29 08:07:19 +0100

    attempt at individual delete buttons

--- app.js
+++ app.js
@@ -7,39 +7,31 @@ document.addEventListener('DOMContentLoaded', () => {
   const quoteList = document.querySelector('.quote-list');
   
   
-  // Extension: Creation of checkbox
-  const box = document.createElement('input');
-  box.setAttribute('type', 'checkbox');
-  box.setAttribute('value', 'highlight');
-  box.setAttribute('name', 'highlight');
-  box.setAttribute('id', 'highlight');
-
-
-  // Extension: Creation of checkbox label
-  const label = document.createElement('label');
-  label.textContent = 'Highlight This Quote!';
-  label.setAttribute('for', 'highlight');
-  
-  
-  // Extension: Adding new label and checkbox
-  form.appendChild(label);
-  form.appendChild(box);
+  // Extension: creation of delete one list item button
+  // can't get to append to each li element
+  const individualDelete = document.createElement('button');
+  individualDelete.textContent = 'Delete This Quote';
+  individualDelete.type = 'button';
   
   
   // Function to add item to the list
   const addListItem = function(quote, character, book, highlight){
     const newItem = document.createElement('li');
     
-    const text = `${quote} <br> &nbsp;&nbsp;&nbsp;
+    const text = `${quote} <br> &nbsp;&nbsp;&nbsp;&nbsp;
     ${character}, &nbsp;<i>${book}</i>`;
     
     if (highlight) {
       newItem.innerHTML = `<b>${text}</b>`;
+      // newItem.appendChild(individualDelete); - not working
+      newItem.setAttribute('id', 'boldQuote');
     }
     else {
       newItem.innerHTML = text;
-      newItem.setAttribute('id', 'normalQuote')
+      // newItem.appendChild(individualDelete); - not working
+      newItem.setAttribute('id', 'normalQuote');
     };
+    
     
     quoteList.appendChild(newItem);
   };
@@ -74,7 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   
   
-  // Creation of delete all but highlighted items button
+  // Creation of selective delete button
   const selectiveDeleteButton = document.createElement('button');
   selectiveDeleteButton.textContent = 'Keep Only Highlighted Quotes';
   selectiveDeleteButton.type = 'button';
@@ -89,6 +81,25 @@ document.addEventListener('DOMContentLoaded', () => {
       quoteList.removeChild(quote);
     });
   });
+  
+  
+  // Extension: Creation of checkbox
+  const box = document.createElement('input');
+  box.setAttribute('type', 'checkbox');
+  box.setAttribute('value', 'highlight');
+  box.setAttribute('name', 'highlight');
+  box.setAttribute('id', 'highlight');
+
+
+  // Extension: Creation of checkbox label
+  const label = document.createElement('label');
+  label.textContent = 'Highlight This Quote!';
+  label.setAttribute('for', 'highlight');
+  
+  
+  // Extension: Adding new label and checkbox
+  form.appendChild(label);
+  form.appendChild(box);
   
   
   
